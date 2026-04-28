8a6553eebb458289ead9f2bbdce2920e33ed4962
Mark Goulding <markgoulding180@gmail.com>
2026-04-28 03:58:54 +0100

    Add cache control headers to prevent caching in predictions API

--- api/predictions.js
+++ api/predictions.js
@@ -27,6 +27,12 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  
+  // Cache control - prevent caching
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
